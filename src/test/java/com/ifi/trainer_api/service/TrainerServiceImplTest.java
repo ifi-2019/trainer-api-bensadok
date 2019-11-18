42bc0d9393f34bc098f0acd2e656bb32c10a3591
Bensadok Imrane <bensadok@b11p6.fil.univ-lille1.fr>
2019-11-18 10:59:54 +0100

    first commit

---
+++ src/test/java/com/ifi/trainer_api/service/TrainerServiceImplTest.java
@@ -0,0 +1,45 @@
+package com.ifi.trainer_api.service;
+
+
+import com.ifi.trainer_api.bo.Trainer;
+import com.ifi.trainer_api.repository.TrainerRepository;
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TrainerServiceImplTest {
+
+    @Test
+    void getAllTrainers_shouldCallTheRepository() {
+        var trainerRepo = mock(TrainerRepository.class);
+        var trainerService = new TrainerServiceImpl(trainerRepo);
+
+        trainerService.getAllTrainers();
+
+        verify(trainerRepo).findAll();
+    }
+
+    @Test
+    void getTrainer_shouldCallTheRepository() {
+        var trainerRepo = mock(TrainerRepository.class);
+        var trainerService = new TrainerServiceImpl(trainerRepo);
+
+        trainerService.getTrainer("Ash");
+
+        verify(trainerRepo).findByName("Ash");
+    }
+
+    @Test
+    void createTrainer_shouldCallTheRepository() {
+        var trainerRepo = mock(TrainerRepository.class);
+        var trainerService = new TrainerServiceImpl(trainerRepo);
+
+        var ash = new Trainer();
+        trainerService.createTrainer(ash);
+
+        verify(trainerRepo).save(ash);
+    }
+
+}
