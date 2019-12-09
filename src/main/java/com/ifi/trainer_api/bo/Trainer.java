da0baf4bcab946fb41730cc5c989fca01207189a
Bensadok Imrane <bensadok@b11p6.fil.univ-lille1.fr>
2019-12-09 10:51:28 +0100

    test security

--- src/main/java/com/ifi/trainer_api/bo/Trainer.java
+++ src/main/java/com/ifi/trainer_api/bo/Trainer.java
@@ -1,5 +1,6 @@
 package com.ifi.trainer_api.bo;
 
+import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -13,6 +14,9 @@ public class Trainer {
 
     @ElementCollection
     private List<Pokemon> team;
+
+    @Column
+    private String password;
 
     public Trainer() {
     }
@@ -36,4 +40,8 @@ public class Trainer {
     public void setTeam(List<Pokemon> team) {
         this.team = team;
     }
+
+    public String getPassword() {return this.password;}
+
+    public void setPassword(String password) {this.password = password;}
 }
